4e14dad83e6be8d7d677bbc9fd62807990446784
lbaitemple <lbai@temple.edu>
2023-03-06 17:32:04 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+pins.LED.onEvent(PinEvent.PulseHigh, function () {
+    mode = mode + 1
+    if (mode > 3) {
+        mode = 0
+    }
+})
+let mode = 0
+let strip = light.createStrip(pins.P28, 30)
+mode = 0
+forever(function () {
+    if (mode == 1) {
+        strip.photonForward(1)
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
